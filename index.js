0800cb9f29894d1c0c67d4544411071efc25e242
hanseji <59394775+hanseji@users.noreply.github.com>
2020-06-03 17:46:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -32,34 +32,9 @@ restService.post("/echo", function(req, res) {
           items: [
             {
               simpleResponse: {
-                textToSpeech: "Here's an example of a browsing carousel."
+                textToSpeech: speech
               }
-            },
-          {
-            "basicCard": {
-              "title": echoText,
-              "subtitle": "This is a subtitle",
-              "formattedText": "This is a basic card.  Text in a basic card can include \"quotes\" and\n    most other unicode characters including emojis.  Basic cards also support\n    some markdown formatting like *emphasis* or _italics_, **strong** or\n    __bold__, and ***bold itallic*** or ___strong emphasis___ as well as other\n    things like line  \nbreaks",
-              "image": {
-                "url": "https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png",
-                "accessibilityText": "Image alternate text"
-              },
-              "buttons": [
-                {
-                  "title": "This is a button",
-                  "openUrlAction": {
-                    "url": "https://assistant.google.com/"
-                  }
-                }
-              ],
-              "imageDisplayOptions": "CROPPED"
             }
-          },
-          {
-            "simpleResponse": {
-              "textToSpeech": "Which response would you like to see next?"
-            }
-          }
           ]
         }
       }
